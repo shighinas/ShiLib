567d453f913e161500fca69a122c2e6dd4c12b77
shighinas <61883009+shighinas@users.noreply.github.com>
2020-12-17 20:07:49 +0530

    Update adminroutes.js

--- src/routes/adminroutes.js
+++ src/routes/adminroutes.js
@@ -6,7 +6,16 @@ var fs = require('fs');
 var path = require('path');
 var multer = require('multer');
  
-var upload = multer({ dest: '/images' });
+var storage = multer.diskStorage({
+    destination: (req, file, cb)=> {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+})
+ 
+var upload = multer({ storage: storage });
 
 
 
